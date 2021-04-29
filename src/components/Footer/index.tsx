5e2e65b1457d4d796df7d936891810421cc7ba15
mars <marswang@trussan.com>
2021-04-29 19:01:18 +0800

    update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import Taro from '@tarojs/taro';
-import { View, Image } from '@tarojs/components';
-
-import wLogo from '@/assets/images/common/w-logo.png';
-
+import { View } from '@tarojs/components';
 import './index.scss'
 
 
-export default function (){
-
+export default function (props){
+  const { text, toList } = props;
   return (
-    <View className='u-footer'>
-      <Image className='u-footer-img' src={wLogo}></Image>
-      <View className='u-footer-desc'>谱写财富蓝图</View>
-      <View className='u-footer-copyright'>Made with ♥ by PlanPro Dev Team</View>
-   </View>
+    <View className="footer">
+      <View>{text}</View>
+      {
+        toList &&
+        <View className="toLast">—— 到底了 ——</View>
+      }
+  </View>
   )
 }
